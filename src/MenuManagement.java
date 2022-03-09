6b0738f7d85ea4227b039d2f6fc1dfdd372a9263
heroPark19 <dnd8848@naver.com>
2022-03-09 22:21:11 +0900

    removed programming.java and added MenuManagement.java

---
+++ src/MenuManagement.java
@@ -0,0 +1,14 @@
+
+public class MenuManagement {
+
+	public static void main(String[] args) {
+		System.out.println("1.Add subject");
+		System.out.println("2.Delete subject");
+		System.out.println("3.Record grade");
+		System.out.println("4.Total grade");
+		System.out.println("5.Grade calculator");
+		System.out.println("6.Exist");
+
+	}
+
+}
