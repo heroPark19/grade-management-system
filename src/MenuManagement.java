3d834daa2135efe500808cb1fd3c03298e6f0483
heroPark19 <dnd8848@naver.com>
2022-03-26 15:51:30 +0900

    add function.java

--- src/MenuManagement.java
+++ src/MenuManagement.java
@@ -17,29 +17,41 @@ public class MenuManagement {
 		System.out.print("Selcet a number : ");
 		num = in.nextInt();
 		
-		switch (num) {
-		case 1:
-			System.out.print("Add your subject :");
-			String sub = in.next();
-			System.out.print("Enter the subject's grade :");
-			double sg = in.nextDouble();
-			break;
-		case 2:
-			System.out.print("Choose the subject you want to delete :");
-			String dsub = in.next();
-			break;
-		case 3:
-			System.out.print("Selcect a subject :");
-			String ssub = in.nextLine();
-			System.out.print("Record your grade :");
-			double csub = in.nextDouble();
-			System.out.printf("%s subject grade is %d.\n", ssub, csub);
-			break;
-		case 4:
-			System.out.print("Your total grade is");
-			break;
+		if (num==1) {
+			addsubject();
+		}
+		else if(num==2) {
+			deletesubject();
+		}
+		else if(num==3) {
+			recordgrade();
+		}
+		else if(num==4) {
+			totalgrade();
 		}
 		}
 	}
-
+	public static void addsubject() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Add your subject :");
+		String sub = in.next();
+		System.out.print("Enter the subject's grade :");
+		double sg = in.nextDouble();
+	}
+	public static void deletesubject() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Choose the subject you want to delete :");
+		String dsub = in.next();
+	}
+	public static void recordgrade() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Selcect a subject :");
+		String ssub = in.nextLine();
+		System.out.print("Record your grade :");
+		double csub = in.nextDouble();
+		System.out.printf("%s subject grade is %d.\n", ssub, csub);
+	}
+	public static void totalgrade() {
+		System.out.println("Your total grade is");
+	}
 }
